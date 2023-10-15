5755c2c11fadd49f813871ef1e56582ce821c973
Gabriel <232036896@aluno.unb.br>
2023-10-15 17:30:53 -0300

    Dando um merge da nova branch preenchida para a main

--- struct.py
+++ struct.py
@@ -1,4 +1,4 @@
 name = input('Nome: ')
 age = int(input('Idade: '))
 
-print(f'Parabéns {name}, você entrou na Struct com {age} anos.')
+print(f'Parabéns {name}, você não entrou na Struct com {age} anos.')
